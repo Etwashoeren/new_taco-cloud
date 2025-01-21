d0f1b34c1c5884a1e982ad238dcefe658d6af6a4
Etwashoeren <sungmin0609@nate.com>
2025-01-21 20:12:49 +0900

    Initial commit

---
+++ src/main/java/sia/new_tacocloud/NewTacoCloudApplication.java
@@ -0,0 +1,13 @@
+package sia.new_tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewTacoCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sia/new_tacocloud/NewTacoCloudApplicationTests.java
@@ -0,0 +1,14 @@
+package sia.new_tacocloud;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
