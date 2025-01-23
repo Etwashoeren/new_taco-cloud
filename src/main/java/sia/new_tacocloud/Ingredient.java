4810b8b314785d5d4e027a9efb15542f18b524d8
Etwashoeren <sungmin0609@nate.com>
2025-01-24 01:17:29 +0900

    refactor. Ingredient 엔티티의 Type 필드에 @Enumerated 추가

--- src/main/java/sia/new_tacocloud/Ingredient.java
+++ src/main/java/sia/new_tacocloud/Ingredient.java
@@ -1,6 +1,8 @@
 package sia.new_tacocloud;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -16,6 +18,7 @@ public class Ingredient {
     @Id
     private final String id;
     private final String name;
+    @Enumerated(EnumType.STRING)
     private final Type type;
 
     public enum Type {
